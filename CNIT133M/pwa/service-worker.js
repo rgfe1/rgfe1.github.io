755033fc45820cd6d3b0fa19c7ee4fde372e2ad8
rgfe1 <31043495+rgfe1@users.noreply.github.com>
2020-11-23 12:26:53 -0800

    Update service-worker.js

--- CNIT133M/pwa/service-worker.js
+++ CNIT133M/pwa/service-worker.js
@@ -1,40 +1,85 @@
-const staticAssets = [
-    './',
-    './styles.css',
-    './app.js'
+var CACHE_VERSION = 'PWA';
+var CACHE_FILES = [
+    'images/offline.png',
+    'app.js',
+    'styles.css'
 ];
 
-self.addEventListener('install', async event => {
-    const cache = await caches.open('static-PWA');
-    cache.addAll(staticAssets);
+self.addEventListener('install', event => {
+    console.log('SW installed');
+    event.waitUntil(
+        caches
+        .open(CACHE_VERSION)
+        .then(cache => {
+            console.log('SW caching files');
+            cache.addAll(CACHE_FILES)
+        })
+        .then(() => self.skipWaiting())
+    );
+});
+
+self.addEventListener('activate', event => {
+    console.log('SW activated');
+    event.waitUntil(
+        caches.keys().then(keyNames => {
+            return Promise.all(
+                keyNames.map(key => {
+                    if(key !== CACHE_VERSION) {
+                        console.log('SW clearing old caches');
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', event => {
-    const {request} = event;
-    const url = new URL(request.url);
-    if(url.origin === location.origin) {
-        event.respondWith(cacheData(request));
-    } else {
-        event.respondWith(networkFirst(request));
-    }
-
+    console.log('SW fetching');
+    event.respondWith(
+        fetch(event.request).catch(() => caches.match(event.request))
+    );
 });
 
-async function cacheData(request) {
-    const cachedResponse = await caches.match(request);
-    return cachedResponse || fetch(request);
-}
 
-async function networkFirst(request) {
-    const cache = await caches.open('dynamic-PWA');
 
-    try {
-        const response = await fetch(request);
-        cache.put(request, response.clone());
-        return response;
-    } catch (error){
-        return await cache.match(request);
+// const staticAssets = [
+//     './',
+//     './styles.css',
+//     './app.js'
+// ];
 
-    }
+// self.addEventListener('install', async event => {
+//     const cache = await caches.open('static-PWA');
+//     cache.addAll(staticAssets);
+// });
 
-}
+// self.addEventListener('fetch', event => {
+//     const {request} = event;
+//     const url = new URL(request.url);
+//     if(url.origin === location.origin) {
+//         event.respondWith(cacheData(request));
+//     } else {
+//         event.respondWith(networkFirst(request));
+//     }
+
+// });
+
+// async function cacheData(request) {
+//     const cachedResponse = await caches.match(request);
+//     return cachedResponse || fetch(request);
+// }
+
+// async function networkFirst(request) {
+//     const cache = await caches.open('dynamic-PWA');
+
+//     try {
+//         const response = await fetch(request);
+//         cache.put(request, response.clone());
+//         return response;
+//     } catch (error){
+//         return await cache.match(request);
+
+//     }
+
+// }
