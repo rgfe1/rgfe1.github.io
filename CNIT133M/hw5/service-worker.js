fe3578b4931bdc77038b9ff1629469630a8da074
rgfe1 <31043495+rgfe1@users.noreply.github.com>
2020-12-13 14:51:16 -0800

    Update service-worker.js

--- CNIT133M/hw5/service-worker.js
+++ CNIT133M/hw5/service-worker.js
@@ -1,52 +1,135 @@
-var CACHE_VERSION = 'Research App Tech';
-var CACHE_FILES = [
+
+// Change to v2, etc. if update any of the local resources, 
+const PRECACHE = 'hw5-v2';
+const RUNTIME = 'runtime';
+
+// A list of local resources to be cached.
+const PRECACHE_URLS = [
        './',
-       './index.html',
+       'index.html',
        'images/lightblue.jpg',
        'images/lightgold.jpg',
        'android-chrome-192x192.png',
        'android-chrome-512x512.png',
        'favicon-16x16.png',
        'favicon-32x32.png',
-       './style.css',
-       './app.js'
+       'style.css',
+       'app.js'
+
+//    'index.html',
+//    './', 
+//    'style.css',
+//    'lightblue.jpg',
+//    'lightgold.jpg',
+//    'manifest.json',
+//    'icon/icon48.png',
+//    'icon/icon180.png',
+//    'icon/icon192.png',
+//    'icon/icon512.png'
 ];
 
+// The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
-    console.log('SW installed');
-    event.waitUntil(
-        caches
-        .open(CACHE_VERSION)
-        .then(cache => {
-            console.log('SW caching files');
-            cache.addAll(CACHE_FILES)
-        })
-        .then(() => self.skipWaiting())
-    );
+   event.waitUntil(
+      caches.open(PRECACHE)
+      .then(cache => cache.addAll(PRECACHE_URLS))
+      .then(self.skipWaiting())
+   );
 });
 
+// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
-    console.log('SW activated');
-    event.waitUntil(
-        caches.keys().then(keyNames => {
-            return Promise.all(
-                keyNames.map(key => {
-                    if(key !== CACHE_VERSION) {
-                        console.log('SW clearing old caches');
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
+   const currentCaches = [PRECACHE, RUNTIME];
+   event.waitUntil(
+      caches.keys().then(cacheNames => {
+      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
+      }).then(cachesToDelete => {
+      return Promise.all(cachesToDelete.map(cacheToDelete => {
+         return caches.delete(cacheToDelete);
+      }));
+      }).then(() => self.clients.claim())
+   );
 });
 
+// The fetch handler serves responses for same-origin resources from a cache.
+// If no response is found, it populates the runtime cache with the response
+// from the network before returning it to the page.
 self.addEventListener('fetch', event => {
-    console.log('SW fetching');
-    event.respondWith(
-        fetch(event.request).catch(() => caches.match(event.request))
-    );
+  // Skip cross-origin requests, like those for Google Analytics if you have one.
+   if (event.request.url.startsWith(self.location.origin)) {
+      event.respondWith(
+         caches.match(event.request).then(cachedResponse => {
+         if (cachedResponse) {
+            return cachedResponse;
+         }
+
+         return caches.open(RUNTIME).then(cache => {
+            return fetch(event.request).then(response => {
+               // Put a copy of the response in the runtime cache.
+               return cache.put(event.request, response.clone()).then(() => {
+               return response;
+               });
+            });
+         });
+         })
+      );
+   }
 });
+
+
+
+
+
+
+// var CACHE_VERSION = 'Research App Tech';
+// var CACHE_FILES = [
+//        './',
+//        './index.html',
+//        'images/lightblue.jpg',
+//        'images/lightgold.jpg',
+//        'android-chrome-192x192.png',
+//        'android-chrome-512x512.png',
+//        'favicon-16x16.png',
+//        'favicon-32x32.png',
+//        './style.css',
+//        './app.js'
+// ];
+
+// self.addEventListener('install', event => {
+//     console.log('SW installed');
+//     event.waitUntil(
+//         caches
+//         .open(CACHE_VERSION)
+//         .then(cache => {
+//             console.log('SW caching files');
+//             cache.addAll(CACHE_FILES)
+//         })
+//         .then(() => self.skipWaiting())
+//     );
+// });
+
+// self.addEventListener('activate', event => {
+//     console.log('SW activated');
+//     event.waitUntil(
+//         caches.keys().then(keyNames => {
+//             return Promise.all(
+//                 keyNames.map(key => {
+//                     if(key !== CACHE_VERSION) {
+//                         console.log('SW clearing old caches');
+//                         return caches.delete(key);
+//                     }
+//                 })
+//             );
+//         })
+//     );
+// });
+
+// self.addEventListener('fetch', event => {
+//     console.log('SW fetching');
+//     event.respondWith(
+//         fetch(event.request).catch(() => caches.match(event.request))
+//     );
+// });
 
 
 
