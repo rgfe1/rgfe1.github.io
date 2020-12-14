e75b4e5cea658d0e232261b1613afa4b248c9b64
rgfe1 <31043495+rgfe1@users.noreply.github.com>
2020-12-13 16:30:19 -0800

    Update service-worker.js

--- CNIT133M/hw5/service-worker.js
+++ CNIT133M/hw5/service-worker.js
@@ -14,18 +14,8 @@ const PRECACHE_URLS = [
        'images/favicon-16x16.png',
        'images/favicon-32x32.png',
        'style.css',
+       'manifest.json',
        'app.js'
-
-//    'index.html',
-//    './', 
-//    'style.css',
-//    'lightblue.jpg',
-//    'lightgold.jpg',
-//    'manifest.json',
-//    'icon/icon48.png',
-//    'icon/icon180.png',
-//    'icon/icon192.png',
-//    'icon/icon512.png'
 ];
 
 // The install handler takes care of precaching the resources we always need.
@@ -75,101 +65,3 @@ self.addEventListener('fetch', event => {
       );
    }
 });
-
-
-
-
-
-
-// var CACHE_VERSION = 'Research App Tech';
-// var CACHE_FILES = [
-//        './',
-//        './index.html',
-//        'images/lightblue.jpg',
-//        'images/lightgold.jpg',
-//        'android-chrome-192x192.png',
-//        'android-chrome-512x512.png',
-//        'favicon-16x16.png',
-//        'favicon-32x32.png',
-//        './style.css',
-//        './app.js'
-// ];
-
-// self.addEventListener('install', event => {
-//     console.log('SW installed');
-//     event.waitUntil(
-//         caches
-//         .open(CACHE_VERSION)
-//         .then(cache => {
-//             console.log('SW caching files');
-//             cache.addAll(CACHE_FILES)
-//         })
-//         .then(() => self.skipWaiting())
-//     );
-// });
-
-// self.addEventListener('activate', event => {
-//     console.log('SW activated');
-//     event.waitUntil(
-//         caches.keys().then(keyNames => {
-//             return Promise.all(
-//                 keyNames.map(key => {
-//                     if(key !== CACHE_VERSION) {
-//                         console.log('SW clearing old caches');
-//                         return caches.delete(key);
-//                     }
-//                 })
-//             );
-//         })
-//     );
-// });
-
-// self.addEventListener('fetch', event => {
-//     console.log('SW fetching');
-//     event.respondWith(
-//         fetch(event.request).catch(() => caches.match(event.request))
-//     );
-// });
-
-
-
-// const staticAssets = [
-//     './',
-//     './styles.css',
-//     './app.js'
-// ];
-
-// self.addEventListener('install', async event => {
-//     const cache = await caches.open('static-PWA');
-//     cache.addAll(staticAssets);
-// });
-
-// self.addEventListener('fetch', event => {
-//     const {request} = event;
-//     const url = new URL(request.url);
-//     if(url.origin === location.origin) {
-//         event.respondWith(cacheData(request));
-//     } else {
-//         event.respondWith(networkFirst(request));
-//     }
-
-// });
-
-// async function cacheData(request) {
-//     const cachedResponse = await caches.match(request);
-//     return cachedResponse || fetch(request);
-// }
-
-// async function networkFirst(request) {
-//     const cache = await caches.open('dynamic-PWA');
-
-//     try {
-//         const response = await fetch(request);
-//         cache.put(request, response.clone());
-//         return response;
-//     } catch (error){
-//         return await cache.match(request);
-
-//     }
-
-// }
